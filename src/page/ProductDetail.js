3afce2a12a4dbb3d3750960a394d4f3ba65de148
darby-95 <160802349+darby-95@users.noreply.github.com>
2024-07-09 12:19:14 +0900

    Update ProductDetail.js

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -13,7 +13,7 @@ function ProductDetail() {
   const [product,setProduct]=useState(null);
   // console.log(id)
   const getProductDetail=async()=>{
-    let url=`http://localhost:3004/products/${id}`;
+    let url=`https://my-json-server.typicode.com/darby-95/react-shop/products/${id}`;
     let response=await fetch(url);
     let data=await response.json();
     console.log(data)
